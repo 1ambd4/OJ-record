08099a559d14686cc4ede954780c25fc05aac6ae
lttzz <luckdog.cdr@gmail.com>
2021-10-19 16:00:36 +0800

    leetcode: 0053 maximum-subarray

--- leetcode/0053.maximum_subarray.cc
+++ leetcode/0053.maximum_subarray.cc
@@ -4,21 +4,24 @@
 using namespace std;
 
 int max_subarray(vector<int>& nums) {
-    vector<int> dp (nums);
-    int result = dp[0];
-
+    int result = nums[0];
+    vector<int> dp(nums);
     for (int i = 1; i < nums.size(); ++i) {
-       dp[i] = max(dp[i], dp[i-1]+nums[i]);
-       if (dp[i] > result) result = dp[i];
+        dp[i] = max(nums[i], dp[i-1]+nums[i]);
+        result = dp[i] > result ? dp[i] : result;
     }
 
     return result;
 }
 
 int main(void) {
-    vector<int> nums_case1 {-2, 1, -3, 4, -1, 2, 1, -5};
+    vector<int> nums_case1 { -2, 1, -3, 4, -1, 2, 1, -5 };
     assert(max_subarray(nums_case1) == 6);
-
+    
+    vector<int> nums_case2 { 5, 4 -1, 7, 8 };
+    assert(max_subarray(nums_case2) == 23);
+    
+    
     cout << "Accepted by the test example!" << endl;
 
     return 0;
