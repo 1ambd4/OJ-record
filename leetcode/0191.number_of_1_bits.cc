fcda3feca86f11835249ad38da54d6ba06665b78
lttzz <luckdog.cdr@gmail.com>
2021-10-31 14:58:14 +0800

    leetcode: 0191 number-of-1-bits

---
+++ leetcode/0191.number_of_1_bits.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int _hamming_weight(uint32_t n)
+{
+    int count = 0;
+    while(n) {
+        if (n&1) {
+            count++;
+        }
+        n >>= 1;
+    }
+    return count;
+}
+
+int hamming_weight(uint32_t n)
+{
+    int count = 0;
+    while (n) {
+        n &= n - 1;
+        count++;
+    }
+    return count;
+}
+
+int main()
+{
+    uint32_t case1 = 15; // 00000000000000000000000000001011L;
+    cout << hamming_weight(case1) << endl;
+
+    return 0;
+}
