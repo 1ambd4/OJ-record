3b240d2263a0287f64108b2a586099020e41bba2
lttzz <luckdog.cdr@gmail.com>
2022-01-01 18:45:50 +0800

    lcof: 06 print-list-in-reverse-order

---
+++ leetcode/coding_interviews/06.print_list_in_reverse_order.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+#include "../leetcode.h"
+
+using namespace std;
+
+void reverse_print(ListNode *head, vector<int> &res);
+
+vector<int> reverse_print(ListNode *head)
+{
+    vector<int> res;
+
+    reverse_print(head, res);
+
+    return res;
+}
+
+void reverse_print(ListNode *head, vector<int> &res)
+{
+    if (head == nullptr) {
+        return ;
+    }
+
+    reverse_print(head->next, res);
+    res.push_back(head->val);
+}
+
+int main()
+{
+    ostream_iterator<int> out_iter(cout, " ");
+
+    vector<int> case1 {1, 3, 2};
+    ListNode *list1 = build_list(case1);
+    show_list(list1->next);
+    vector<int> res1 = reverse_print(list1->next);
+    copy(res1.begin(), res1.end(), out_iter);
+    cout << endl;
+
+    vector<int> case2 { };
+    ListNode *list2 = build_list(case2);
+    show_list(list2->next);
+    vector<int> res2 = reverse_print(list2->next);
+    copy(res2.begin(), res2.end(), out_iter);
+    cout << endl;
+
+    return 0;
+}
