0fd41811afb8f75d2d65772ed554f93877427a9b
lttzz <luckdog.cdr@gmail.com>
2022-03-04 15:04:40 +0800

    leetcode.h

--- leetcode/leetcode.h
+++ leetcode/leetcode.h
@@ -1,16 +1,17 @@
 #ifndef LEETCODE_H_
 #define LEETCODE_H_
 
+#include <queue>
 #include <vector>
 
 struct ListNode {
-public:
-    ListNode() : val(0), next(nullptr) { }
-    ListNode(int x) : val(x), next(nullptr) { };
-    ListNode(int x, ListNode *next) : val(x), next(next) { };
+    public:
+        ListNode() : val(0), next(nullptr) { }
+        ListNode(int x) : val(x), next(nullptr) { };
+        ListNode(int x, ListNode *next) : val(x), next(next) { };
 
-    int val;
-    ListNode *next;
+        int val;
+        ListNode *next;
 };
 
 ListNode* build_list(std::vector<int> nums)
@@ -34,4 +35,81 @@ void show_list(ListNode *head)
     std::cout  << std::endl;
 }
 
+struct TreeNode {
+    public:
+        TreeNode() : val(0), left(nullptr), right(nullptr) { }
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) { }
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) { }
+
+        int val;
+        TreeNode *left, *right;
+};
+
+// 1, null, 2, 3 层次遍历建树
+TreeNode* build_tree(std::vector<int>& nums)
+{
+    if (nums.size() == 0) return nullptr;
+    TreeNode *head = new TreeNode(nums[0]), *cur = head, *temp;
+    std::queue<TreeNode*> q;
+    q.push(cur);
+    for (int i = 1; i < nums.size(); i+=2) {
+        cur = q.front();
+        q.pop();
+        if (i < nums.size()) {
+            if (nums[i] != -1) {
+                temp = new TreeNode(nums[i]);
+                cur->left = temp;
+                q.push(temp);
+            } else {
+                cur->left = nullptr;
+            }
+        }
+        if (i+1 < nums.size()) {
+            if (nums[i+1] != -1) {
+                temp = new TreeNode(nums[i+1]);
+                cur->right = temp;
+                q.push(temp);
+            } else {
+                cur->right = nullptr;
+            }
+        }
+    }
+    return head;
+}
+
+// 层序遍历
+void traversal(TreeNode *head)
+{
+    if (head == nullptr) return ;
+    TreeNode *cur;
+    std::queue<TreeNode*> q;
+    q.push(head);
+    std::cout << head->val << " ";
+    while (!q.empty()) {
+        cur = q.front();
+        q.pop();
+        if (cur->left != nullptr) {
+            q.push(cur->left);
+            std::cout << cur->left->val << " ";
+        } else {
+            std::cout << "null ";
+        }
+        if (cur->right != nullptr) {
+            q.push(cur->right);
+            std::cout << cur->right->val << " ";
+        } else {
+            std::cout << "null ";
+        }
+    }
+    std::cout << std::endl;
+}
+
+template<typename T>
+void print_vector(std::vector<T> vec)
+{
+    for (const auto &it : vec) {
+        std::cout << it << " ";
+    }
+    std::cout << std::endl;
+}
 #endif  // LEETCODE_H
