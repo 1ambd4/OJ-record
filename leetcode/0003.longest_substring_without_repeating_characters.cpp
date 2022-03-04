b7dd40e1f85ae7427d79709e088d4e434fd11a36
lttzz <luckdog.cdr@gmail.com>
2022-03-04 15:35:24 +0800

    leetcode: 0003 longest-substring-without-repeating-characters

--- leetcode/0003.longest_substring_without_repeating_characters.cpp
+++ leetcode/0003.longest_substring_without_repeating_characters.cpp
@@ -4,24 +4,25 @@
 
 using namespace std;
 
-// 滑动窗口
 int length_of_longest_substring(string s)
 {
     int left = 0, right = 0, res = 0;
     unordered_map<char, int> window;
 
     while (right < s.size()) {
-        char c = s[right];
-        ++right;
-        ++window[c];
+        char c = s[right++];
+        window[c]++;
 
+        // 当前字符添加后，若出现重复字符，则将移动左窗口收束左边界
         while (window[c] > 1) {
-            char d = s[left];
-            ++left;
-            --window[d];
+            char c = s[left++];
+            window[c]--;
         }
 
-        res = max(res, right - left);
+        // 因为此题左边界收束的条件不同
+        // 因而答案的更新位置不同于其他滑动窗口题
+        // 要在左边界收束完成之后才能保证无重复字符
+        res = max(res, right-left);
     }
 
     return res;
