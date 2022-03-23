4ac5e9b69a9b2e5e7312e86682e985ec060df4d2
lttzz <luckdog.cdr@gmail.com>
2022-03-23 16:59:36 +0800

    leetcode: 0104 maximum-depth-of-binary-tree

--- leetcode/0104.maximum_depth_of_binary_tree.cpp
+++ leetcode/0104.maximum_depth_of_binary_tree.cpp
@@ -3,33 +3,32 @@
 
 using namespace std;
 
-int maxDepth(TreeNode *root)
-{
-    if (root == nullptr) {
-        return 0;
-    }
-    return max(maxDepth(root->left), maxDepth(root->right)) + 1;
-}
+// int maxDepth(TreeNode *root)
+// {
+//     if (root == nullptr) {
+//         return 0;
+//     }
+//     return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+// }
 
 // 回溯
-void traverse(TreeNode *root, int &depth, int &res);
-int max_depth(TreeNode *root)
+void traverse(TreeNode *root, int& depth, int& res)
+{
+    if (root == nullptr) {
+        res = max(depth, res);
+        return;
+    }
+
+    ++depth;
+    traverse(root->left, depth, res);
+    traverse(root->right, depth, res);
+    --depth;
+}
+int maxDepth(TreeNode *root)
 {
     int depth = 0, res = 0;
     traverse(root, depth, res);
     return res;
-}
-void traverse(TreeNode *root, int &depth, int &res)
-{
-    if (root == nullptr) {
-        return ;
-    }
-
-    ++depth;
-    res = max(res, depth);
-    traverse(root->left, depth, res);
-    traverse(root->right, depth, res);
-    --depth;
 }
 
 int main()
@@ -37,6 +36,5 @@ int main()
     vector<int> case1 {3, 9, 20, -1, -1, 15, 7};
     TreeNode *tree1 = build_tree(case1);
     cout << maxDepth(tree1) << endl;
-    cout << max_depth(tree1) << endl;
     return 0;
 }
