b34d0efc08a1784c58ba2045b66ad61263917805
lttzz <luckdog.cdr@gmail.com>
2022-03-23 22:26:21 +0800

    leetcode: 0797 all-paths-from-source-to-target

---
+++ leetcode/0797.all_paths_from_source_to_target.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void traverse(vector<vector<int>>& graph, vector<vector<int>>& res,
+        vector<int>& path, int s)
+{
+    path.push_back(s);
+
+    int n = graph.size();
+    if (s == n-1) {
+        res.push_back(path);
+        path.pop_back();
+        return ;
+    }
+
+    for (const int& vertex : graph[s]) {
+        traverse(graph, res, path, vertex);
+    }
+
+    path.pop_back();
+}
+
+vector<vector<int>> all_paths_source_target(vector<vector<int>>& graph)
+{
+    vector<vector<int>> res;
+    vector<int> path;
+    traverse(graph, res, path, 0);
+    return res;
+}
+
+int main()
+{
+    vector<vector<int>> graph1{{1,2},{3},{3},{}};
+    vector<vector<int>> res1 = all_paths_source_target(graph1);
+    for (const auto& vertex : res1) {
+        for (const auto& e : vertex) {
+            cout << e << ',';
+        }
+        cout << '#';
+    }
+    cout << endl;
+
+    vector<vector<int>> graph2{{4,3,1},{3,2,4},{3},{4},{}};
+    vector<vector<int>> res2 = all_paths_source_target(graph2);
+    for (const auto& vertex: res2) {
+        for (const auto& e : vertex) {
+            cout << e << ',';
+        }
+        cout << '#';
+    }
+    cout << endl;
+    return 0;
+}
