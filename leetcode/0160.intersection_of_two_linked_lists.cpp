7c5b0e1590d3a06d13dc18ca900beabcf2accf60
lttzz <luckdog.cdr@gmail.com>
2022-05-06 23:01:46 +0800

    leetcode: 0160 intersection-of-two-linked-lists

---
+++ leetcode/0160.intersection_of_two_linked_lists.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+#include "leetcode.h"
+
+using namespace std;
+
+ListNode* get_intersection_node(ListNode *head_a, ListNode *head_b)
+{
+    ListNode *la = head_a, *lb = head_b;
+    while (la != lb) {
+        if (la == nullptr) {
+            la = head_b;
+        } else {
+            la = la->next;
+        }
+        if (lb == nullptr) {
+            lb = head_a;
+        } else {
+            lb = lb->next;
+        }
+    }
+    return la;
+}
+
+int main()
+{
+    vector<int> vec1 { 4, 1, 8, 4, 5 }, vec2 { 5, 6, 1, 8, 4, 5 };
+    ListNode *list1 = build_list(vec1), *list2 = build_list(vec2);
+    ListNode *res1 = get_intersection_node(list1->next, list2->next);
+    show_list(list1->next);
+    show_list(list2->next);
+    show_list(res1);
+    if (res1 == nullptr) cout << "fjdsslfj" << endl;
+    return 0;
+}
