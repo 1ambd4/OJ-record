c3a2e47808b79bda41a54dbfc0a1288c6c1475b0
lttzz <luckdog.cdr@gmail.com>
2022-08-10 00:10:48 +0800

    leetcode: 0583 delete-operation-for-two-strings

--- leetcode/0583.delete_operation_for_two_strings.cpp
+++ leetcode/0583.delete_operation_for_two_strings.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-// 最长公共子序列
-int MinimumDistance(string word1, string word2)
+// 删完最后两个单词应当都只剩下最长公共子序列部分
+// dp[i][j]: 以word1[i]和word2[j]为结尾的最长公共子序列
+
+int minimum_distance(string word1, string word2)
 {
-    int dp[word1.size()+1][word2.size()+1];
-    memset(dp, 0, sizeof(dp));
-    for (int i = 1; i <= word1.size(); ++i) {
-        for (int j = 1; j <= word2.size(); ++j) {
-             if (word1[i-1] == word2[j-1]) {
-                 dp[i][j] = dp[i-1][j-1] + 1;
-             } else {
-                 dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-             }
+    int n = word1.size(), m = word2.size();
+
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            if (word1[i-1] == word2[j-1]) {
+                dp[i][j] = dp[i-1][j-1] + 1;
+            } else {
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+            }
         }
     }
-    return word1.size() + word2.size() - 2*dp[word1.size()][word2.size()];
+
+    return n + m - 2 * dp[n][m];
 }
 
 int main(void)
 {
-    cout << MinimumDistance("sea", "eat") << endl;
-    cout << MinimumDistance("leetcode", "etco") << endl;
+    cout << minimum_distance("sea", "eat") << endl;
+    cout << minimum_distance("leetcode", "etco") << endl;
     // 是子序列啊，不是子串
-    cout << MinimumDistance("park", "spake") << endl;
-    cout << MinimumDistance("intention", "execution") << endl;
+    cout << minimum_distance("park", "spake") << endl;
+    cout << minimum_distance("intention", "execution") << endl;
     return 0;
 }
