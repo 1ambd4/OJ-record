dfc07d4afc15facd2cc6bed1bde6ae211b2a2c3e
lttzz <luckdog.cdr@gmail.com>
2022-09-17 16:53:34 +0800

    leetcode: 0198 house-robber

--- leetcode/0198.house_robber.cpp
+++ leetcode/0198.house_robber.cpp
@@ -3,6 +3,23 @@
 
 using namespace std;
 
+int rob_(vector<int>& nums)
+{
+    int n = nums.size();
+
+    if (n == 1) return nums[0];
+    vector<vector<int>> f(n, vector<int>(2, 0));
+    f[0][0] = 0, f[0][1] = nums[0];
+
+    for (int i = 1; i < n; ++i) {
+        f[i][0] = max(f[i-1][0], f[i-1][1]);
+        f[i][1] = f[i-1][0] + nums[i];
+    }
+
+    return max(f[n-1][0], f[n-1][1]);
+}
+
+
 // dp[i]: 到第i号房间为止，可偷得的最高金额
 
 int rob(vector<int>& nums)
