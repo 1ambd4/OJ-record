e11e07b7ecccbf234158cab5851bd2fd266e53db
lttzz <luckdog.cdr@gmail.com>
2023-03-03 19:07:09 +0800

    leetcode: 0875 koko-eating-bananas

--- leetcode/0875.koko_eating_bananas.cpp
+++ leetcode/0875.koko_eating_bananas.cpp
@@ -6,7 +6,32 @@
 using namespace std;
 
 // h = f(k)，易知f(k)是单调不增的
-int f(vector<int> &piles, int k)
+int f(vector<int>& piles, int k)
+{
+    int res = 0;
+    for (const auto& pile : piles) {
+        res += pile / k;
+        if (pile % k != 0) ++res;
+    }
+    return res;
+
+}
+
+// 寻找f(k)=h的最小k
+int min_eating_speed(vector<int>& piles, int h)
+{
+    int l = 1, r = 1e9l + 1;
+    while (l < r) {
+        int m = (l + r) >> 1;
+        if (f(piles, m) <= h) r = m;
+        else l = m + 1;
+    }
+
+    return l;
+}
+
+
+int _f(vector<int> &piles, int k)
 {
     int hours = 0;
     for (const int &pile : piles) {
@@ -18,8 +43,7 @@ int f(vector<int> &piles, int k)
     return hours;
 }
 
-// 寻找f(k)=h的最小k
-int min_eating_speed(vector<int> &piles, int h)
+int _min_eating_speed(vector<int> &piles, int h)
 {
     int left = 1, right = 1e9+1;
 
