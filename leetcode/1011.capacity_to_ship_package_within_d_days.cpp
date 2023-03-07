f831cad19d60d4557735cedffec98750f48a1835
lttzz <luckdog.cdr@gmail.com>
2023-03-07 19:50:10 +0800

    leetcode: 1011 capacity-to-ship-package-within-d-days

--- leetcode/1011.capacity_to_ship_package_within_d_days.cpp
+++ leetcode/1011.capacity_to_ship_package_within_d_days.cpp
@@ -3,8 +3,44 @@
 
 using namespace std;
 
+int ship_within_days(vector<int> &weights, int days)
+{
+    auto f = [&weights](int x) -> int {
+        int res = 0;
+
+        for (int i = 0; i < weights.size(); ) {
+            int cap = x;
+
+            while (i < weights.size()) {
+                if (cap < weights[i]) break;
+                else cap -= weights[i];
+                ++i;
+            }
+
+            ++res;
+        }
+
+        return res;
+    };
+
+    int l = 0, r = 0;
+    for (const auto& weight : weights) {
+        l = max(l, weight);
+        r += weight;
+    }
+
+    while (l < r) {
+        int m = (l + r) >> 1;
+        if (f(m) <= days) r = m;
+        else l = m + 1;
+    }
+
+    return l;
+}
+
+
 // f(x)=days
-int f(vector<int> &weights, int x)
+int _f(vector<int> &weights, int x)
 {
     int days = 0, tmp = 0;
     for (int i = 0; i < weights.size(); ) {
@@ -42,7 +78,7 @@ int f(vector<int> &weights, int x)
 // }
 
 // 寻找f(s)=day的最小s
-int ship_within_days(vector<int> &weights, int days)
+int _ship_within_days(vector<int> &weights, int days)
 {
     int left = 0, right = 0 + 10;
     for (const auto &weight : weights) {
@@ -52,11 +88,11 @@ int ship_within_days(vector<int> &weights, int days)
 
     while (left <= right) {
         int mid = left + (right - left) / 2;
-        if (f(weights, mid) == days) {
+        if (_f(weights, mid) == days) {
             right = mid -1;
-        } else if (f(weights, mid) > days) {
+        } else if (_f(weights, mid) > days) {
             left = mid + 1;
-        } else if (f(weights, mid) < days) {
+        } else if (_f(weights, mid) < days) {
             right = mid -1;
         }
     }
@@ -68,9 +104,12 @@ int main()
 {
     vector<int> w1 { 1,2,3,4,5,6,7,8,9,10 };
     cout << ship_within_days(w1, 5) << endl;
+
     vector<int> w2 { 3,2,2,4,1,4 };
     cout << ship_within_days(w2, 3) << endl;
+
     vector<int> w3 { 1,2,3,1,1 };
     cout << ship_within_days(w3, 4) << endl;
+
     return 0;
 }
