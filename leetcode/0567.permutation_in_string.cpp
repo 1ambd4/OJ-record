6d555ae9abb87b08fc9c943cd7681f72792b3027
lttzz <luckdog.cdr@gmail.com>
2023-03-07 21:16:25 +0800

    leetcode: 0567 permutation-in-string

--- leetcode/0567.permutation_in_string.cpp
+++ leetcode/0567.permutation_in_string.cpp
@@ -4,8 +4,37 @@
 
 using namespace std;
 
-// s1 是模式串，s2是主串
 bool check_inclusion(string s1, string s2)
+{
+    int left = 0, right = 0, valid = 0;
+    unordered_map<char, int> need, window;
+
+    for (const auto& c : s1) need[c]++;
+
+    while (right < s2.size()) {
+        char c = s2[right++];
+        if (need.count(c)) {
+            window[c]++;
+            if (window[c] == need[c]) ++valid;
+        }
+
+        while (valid == need.size()) {
+            if (right - left == s1.size()) return true;
+
+            char c = s2[left++];
+            if (need.count(c)) {
+                if (window[c] == need[c]) --valid;
+                window[c]--;
+            }
+        }
+    }
+
+    return false;
+}
+
+
+// s1 是模式串，s2是主串
+bool _check_inclusion(string s1, string s2)
 {
     int left = 0, right = 0, valid = 0;
     unordered_map<char, int> need, window;
@@ -28,7 +57,7 @@ bool check_inclusion(string s1, string s2)
             // 更新答案
             if (right-left == s1.size()) {
                 return true;
-            } 
+            }
 
             // 左缩
             char c = s2[left++];
@@ -48,7 +77,9 @@ int main()
 {
     string s1 = "eidbaooo", t1 = "ab";
     cout << check_inclusion(t1, s1) << endl;
+
     string s2 = "eidboall", t2 = "ba";
     cout << check_inclusion(t2, s2) << endl;
+
     return 0;
 }
