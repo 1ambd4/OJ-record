0722b75e1dc1a7a673b7bfe567d896dedf3ece70
lttzz <luckdog.cdr@gmail.com>
2023-03-08 17:10:24 +0800

    leetcode: 0141 linked-list-cycle

--- leetcode/0141.linked_list_cycle.cpp
+++ leetcode/0141.linked_list_cycle.cpp
@@ -4,8 +4,20 @@
 
 using namespace std;
 
-// 快慢指针判断链表是否有环
 bool has_cycle(ListNode *head)
+{
+    ListNode *slow = head, *fast = head;
+    while (fast != nullptr || fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) return true;
+    }
+    return false;
+}
+
+
+// 快慢指针判断链表是否有环
+bool _has_cycle(ListNode *head)
 {
     ListNode *slow = head, *fast = head;
     do {
@@ -23,7 +35,8 @@ int main()
 {
     vector<int> nums1 { 3, 2, 0, -4 };
     ListNode *case1 = build_list(nums1);
-    case1->next->next->next->next->next = case1->next->next;
-    cout << has_cycle(case1->next) << endl;
+    case1->next->next->next->next = case1->next;
+    cout << has_cycle(case1) << endl;
+
     return 0;
 }
