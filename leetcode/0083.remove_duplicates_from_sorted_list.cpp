7017e703cbd62c2ebb034af38d544a7ffde4efa3
lttzz <luckdog.cdr@gmail.com>
2023-03-08 21:24:05 +0800

    leetcode: 0083 remove-duplicates-from-sorted-list

--- leetcode/0083.remove_duplicates_from_sorted_list.cpp
+++ leetcode/0083.remove_duplicates_from_sorted_list.cpp
@@ -4,27 +4,76 @@
 
 using namespace std;
 
+// 大概就是line 33算法的简化版，省去了一个指针
 ListNode* delete_duplicates(ListNode *head)
 {
+    ListNode *cur = head;
+    while (cur != nullptr && cur->next != nullptr) {
+        cur->next->val == cur->val ? cur->next = cur->next->next : cur = cur->next;
+    }
+    return head;
+}
+
+ListNode* _delete_duplicates(ListNode *head)
+{
     if (head == nullptr) return nullptr;
-    ListNode *left = head, *right = head;
-    while (right != nullptr) {
-        if (right->val != left->val) {
-            left->next = right;
-            left = left->next;
+
+    ListNode *dummy = new ListNode(-1, head), *slow = head, *fast = head;
+    while (fast != nullptr) {
+        fast = fast->next;
+        while (fast != nullptr && fast->val == slow->val) {
+            fast = fast->next;
         }
-        right = right->next;
+        slow->next = fast;
+        slow = slow->next;
+    }
+    return dummy->next;
+}
+
+ListNode* __delete_duplicates(ListNode *head)
+{
+    if (head == nullptr) return nullptr;
+
+    ListNode *slow = head, *fast = head;
+    while (fast != nullptr) {
+        if (fast->val != slow->val) {
+            slow->next = fast;
+            slow = slow->next;
+        }
+        fast = fast->next;
+    }
+    slow->next = nullptr;
+
+    return head;
+}
+
+ListNode* ___delete_duplicates(ListNode *head)
+{
+    if (head == nullptr) return nullptr;
+    ListNode *slow = head, *fast = head;
+    while (fast != nullptr) {
+        if (fast->val != slow->val) {
+            slow->next = fast;
+            slow = slow->next;
+        }
+        fast = fast->next;
     }
 
-    left->next = nullptr;
+    slow->next = nullptr;
     return head;
 }
 
 int main()
 {
-    vector<int> vec1 { 1,1,2 };
+    vector<int> vec1 {1,1,2};
     ListNode *l1 = build_list(vec1);
     delete_duplicates(l1);
     show_list(l1);
+
+    vector<int> vec2 {1,1,2,3,3};
+    ListNode *l2 = build_list(vec2);
+    delete_duplicates(l2);
+    show_list(l2);
+
     return 0;
 }
