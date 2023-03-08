797a9caa966d54bac32c35f344ff438b26890a29
lttzz <luckdog.cdr@gmail.com>
2023-03-08 22:06:50 +0800

    leetcode: 0303 range-sum-query-immutable

--- leetcode/0303.range_sum_query_immutable.cpp
+++ leetcode/0303.range_sum_query_immutable.cpp
@@ -5,7 +5,28 @@ using namespace std;
 
 class NumArray {
 public:
-    NumArray(vector<int> &nums)
+    NumArray(vector<int>& nums)
+    {
+        pre_sum.resize(nums.size() + 1);
+        pre_sum[0] = 0;
+        for (int i = 1; i <= pre_sum.size(); ++i) {
+            pre_sum[i] = pre_sum[i-1] + nums[i-1];
+        }
+    }
+
+    int sum_range(int left, int right)
+    {
+        return pre_sum[right+1] - pre_sum[left];
+    }
+
+private:
+    vector<int> pre_sum;
+};
+
+
+class _NumArray {
+public:
+    _NumArray(vector<int> &nums)
     {
         preSum.resize(nums.size()+1);
         preSum[0] = 0;
@@ -27,8 +48,8 @@ int main()
 {
     vector<int> vec1 { -2, 0, 3, -5, 2, -1 };
     NumArray *case1 = new NumArray(vec1);
-    cout << case1->sum_range(0,2) << ", "
-        << case1->sum_range(2,5) << ", "
-        << case1->sum_range(0,5) << endl;
+    cout << case1->sum_range(0,2) << endl;
+    cout << case1->sum_range(2,5) << endl;
+    cout << case1->sum_range(0,5) << endl;
     return 0;
 }
