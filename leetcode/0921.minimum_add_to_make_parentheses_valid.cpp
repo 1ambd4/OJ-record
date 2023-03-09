ad0cf5b58e50230df251c9de916cdaaefbfe3321
lttzz <luckdog.cdr@gmail.com>
2023-03-09 15:51:04 +0800

    leetcode: 0921 minimum-add-to-make-parentheses-valid

--- leetcode/0921.minimum_add_to_make_parentheses_valid.cpp
+++ leetcode/0921.minimum_add_to_make_parentheses_valid.cpp
@@ -6,6 +6,42 @@ using namespace std;
 
 int min_add_to_make_valid(string s)
 {
+    int res = 0, need = 0;
+
+    for (const auto& c : s) {
+        if (c == '(') {
+            need++;
+        } else {
+            need--;
+            if (need ==  -1) {
+                res++;
+                need = 0;
+            }
+        }
+    }
+
+    return res + need;
+}
+
+// 无法处理"()))(("
+int __min_add_to_make_valid(string s)
+{
+    int res = 0;
+
+    for (const auto& c : s) {
+        if (c == '(') {
+            res++;
+        } else if (c == ')') {
+            res--;
+            if (res == -1) res =  1;
+        }
+    }
+
+    return res;
+}
+
+int _min_add_to_make_valid(string s)
+{
     stack<char> stk;
     int res = 0;
     for (const char& ch : s) {
@@ -27,5 +63,8 @@ int main()
     cout << min_add_to_make_valid(s1) << endl;
     string s2 {"((("};
     cout << min_add_to_make_valid(s2) << endl;
+    string s3 {"()))(("};
+    cout << min_add_to_make_valid(s3) << endl;
+
     return 0;
 }
