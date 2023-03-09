46c0d4f3f4e55d0491d70a76125eb98b538fa75b
lttzz <luckdog.cdr@gmail.com>
2023-03-09 15:37:00 +0800

    leetcode: 0020 valid-parentheses

--- leetcode/0020.valid_parentheses.cpp
+++ leetcode/0020.valid_parentheses.cpp
@@ -6,6 +6,40 @@ using namespace std;
 bool is_valid(string s)
 {
     stack<char> stk;
+
+    auto left = [](char c) ->char {
+        switch(c) {
+            case ')':
+                return '(';
+            case ']':
+                return '[';
+            case '}':
+                return '{';
+        }
+    };
+
+    for (const auto& c : s) {
+        if (c == '(' || c == '[' || c == '{') {
+            stk.push(c);
+        } else {
+            if (stk.empty() || stk.top() != left(c)) {
+                return false;
+            } else {
+                stk.pop();
+            }
+        }
+    }
+
+    if (!stk.empty()) {
+        return false;
+    }
+
+    return true;
+}
+
+bool _is_valid(string s)
+{
+    stack<char> stk;
     for (const char& ch : s) {
         if (ch == '(') stk.push(')');
         else if (ch == '{') stk.push('}');
@@ -33,5 +67,6 @@ int main()
     cout << is_valid("(]") << endl;
     cout << is_valid("[") << endl;
     cout << is_valid("]") << endl;
+
     return 0;
 }
