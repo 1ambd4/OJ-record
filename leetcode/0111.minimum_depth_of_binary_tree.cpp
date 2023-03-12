7228d16b0be6e2f3d05dc70a16a7fb2664c0d42b
lttzz <luckdog.cdr@gmail.com>
2023-03-12 11:02:38 +0800

    leetcode: 0111 minimum-depth-of-binary-tree

--- leetcode/0111.minimum_depth_of_binary_tree.cpp
+++ leetcode/0111.minimum_depth_of_binary_tree.cpp
@@ -6,9 +6,42 @@
 
 using namespace std;
 
+
+int minimum_depth(TreeNode *root)
+{
+    int depth = 0;
+    if (root == nullptr) return depth;
+
+    queue<TreeNode*> q;
+    q.push(root);
+    TreeNode *cur = nullptr;
+
+    while (!q.empty()) {
+        int n = q.size();
+        ++depth;
+        for (int i = 0; i < n; ++i) {
+            cur = q.front();
+            q.pop();
+
+            // 我太蠢啦
+            // if (root->left == nullptr && root->right == nullptr) {
+            // if (cur->left == nullptr && cur->right == nullptr) {
+            if (cur->left == cur->right) {
+                return depth;
+            }
+
+            if (cur->left != nullptr) q.push(cur->left);
+            if (cur->right != nullptr) q.push(cur->right);
+        }
+    }
+
+    return -1;
+}
+
+
 // 层次遍历（BFS）
-// 错误写法
-int _minimum_depth(TreeNode *root)
+// 错误写法（笑死，以前好蠢啊我
+int __minimum_depth(TreeNode *root)
 {
     if (root == nullptr) return 0;
     queue<TreeNode*> q;
@@ -30,7 +63,7 @@ int _minimum_depth(TreeNode *root)
     }
 }
 
-int minimum_depth(TreeNode *root)
+int _minimum_depth(TreeNode *root)
 {
     if (root == nullptr)  return 0;
     queue<TreeNode*> q;
@@ -61,17 +94,17 @@ int main()
 {
     vector<int> nums1 { 3, 9, 20, -1, -1, 15, 7 };
     TreeNode *case1 = build_tree(nums1);
-    traversal(case1);
+    // traversal(case1);
     cout << minimum_depth(case1) << endl;
 
     vector<int> nums2 { 2, -1, 3, -1, 4, -1, 5, -1, 6 };
     TreeNode *case2 = build_tree(nums2);
-    traversal(case2);
+    // traversal(case2);
     cout << minimum_depth(case2) << endl;
 
     vector<int> nums3 { 1, 2, 3, 4, 5 };
     TreeNode *case3 = build_tree(nums3);
-    traversal(case3);
+    // traversal(case3);
     cout << minimum_depth(case3) << endl;
     return 0;
 }
