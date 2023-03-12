46777881050d3238f9906b38878983ff0a79b00e
lttzz <luckdog.cdr@gmail.com>
2023-03-12 10:07:29 +0800

    leetcode: 0105 construct-binary-tree-from-preorder-and-inorder-traversal

--- leetcode/0105.construct_binary_tree_from_preorder_and_inorder_traversal.cpp
+++ leetcode/0105.construct_binary_tree_from_preorder_and_inorder_traversal.cpp
@@ -5,11 +5,48 @@
 
 using namespace std;
 
-TreeNode *build(vector<int>& preorder, int plo, int phi,
+// build
+// 二叉树递归的问题，要往左右子树上想
+// 这里的话，就是拿到rootval构建根节点之后递归的构造左右子树
+// 那么，构造左右子树的过程实际上和构造根节点的过程是一样的
+// 便将此定义为递归函数
+TreeNode* build(vector<int>& preorder, int plo, int phi,
+        vector<int>& inorder, int ilo, int ihi)
+{
+    if (plo > phi) return nullptr;
+
+    // 在inorder里找到rootval，则rootval将序列分成左右两边
+    int rootval = preorder[plo], index = 0;
+    for (int i = ilo; i <= ihi; ++i) {
+        if (inorder[i] == rootval) {
+            index = i;
+            break;
+        }
+    }
+
+    int leftsize = index - ilo;
+
+    TreeNode *root = new TreeNode(rootval);
+    root->left = build(preorder, plo+1, plo+leftsize,
+                    inorder, ilo, index-1);
+    root->right = build(preorder, plo+leftsize+1, phi,
+                    inorder, index+1, ihi);
+    return root;
+}
+
+TreeNode* build(vector<int>& preorder, vector<int>& inorder)
+{
+    return build(preorder, 0, preorder.size()-1,
+            inorder, 0, inorder.size()-1);
+}
+
+
+
+TreeNode* _build(vector<int>& preorder, int plo, int phi,
                 vector<int>& inorder, int ilo, int ihi)
 {
     if (plo > phi) return nullptr;
-    
+
     // 找到根在中序序列中的索引
     int rootval = preorder[plo], index = 0;
     for (int i = ilo; i <= ihi; ++i) {
@@ -17,7 +54,7 @@ TreeNode *build(vector<int>& preorder, int plo, int phi,
             index = i;
             break;
         }
-    } 
+    }
 
     TreeNode *root = new TreeNode(preorder[plo]);
     root->left = build(preorder, plo+1, plo+index-ilo,
@@ -28,7 +65,7 @@ TreeNode *build(vector<int>& preorder, int plo, int phi,
     return root;
 }
 
-TreeNode* build(vector<int>& preorder, vector<int>& inorder)
+TreeNode* _build(vector<int>& preorder, vector<int>& inorder)
 {
     return build(preorder, 0, preorder.size()-1,
             inorder, 0, inorder.size()-1);
