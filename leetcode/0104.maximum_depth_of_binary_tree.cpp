74baccf355826d4a946b452958ce6203e788226e
lttzz <luckdog.cdr@gmail.com>
2023-03-12 09:19:23 +0800

    leetcode: 0104 maximum-depth-of-binary-tree

--- leetcode/0104.maximum_depth_of_binary_tree.cpp
+++ leetcode/0104.maximum_depth_of_binary_tree.cpp
@@ -1,7 +1,66 @@
 #include <iostream>
+#include <queue>
+
 #include "leetcode.h"
 
 using namespace std;
+
+// 动态规划（自底向上）
+int max_depth(TreeNode *root)
+{
+    if (root == nullptr) return 0;
+
+    int left = max_depth(root->left);
+    int right = max_depth(root->right);
+
+    return 1 + max(left, right);
+}
+
+// 回溯（自顶向下）
+void traverse(TreeNode *root, int depth, int &res)
+{
+    if (root == nullptr) {
+        res = max(res, depth);
+        return ;
+    }
+
+    ++depth;
+    traverse(root->left, depth, res);
+    traverse(root->right, depth, res);
+    --depth;
+}
+
+int _max_depth(TreeNode* root)
+{
+    int res = 0, depth = 0;
+    traverse(root, depth, res);
+    return res;
+}
+
+
+// 层次遍历
+int __max_depth(TreeNode *root)
+{
+    int res = 0;
+    if (root == nullptr) return res;
+
+    queue<TreeNode*> q;
+    q.push(root);
+    TreeNode *cur = nullptr;
+
+    while (!q.empty()) {
+        int n = q.size();
+        for (int i = 0; i < n; ++i) {
+            cur = q.front();
+            q.pop();
+            if (cur->left != nullptr) q.push(cur->left);
+            if (cur->right != nullptr) q.push(cur->right);
+        }
+        ++res;
+    }
+
+    return res;
+}
 
 // int maxDepth(TreeNode *root)
 // {
@@ -12,7 +71,7 @@ using namespace std;
 // }
 
 // 回溯
-void traverse(TreeNode *root, int& depth, int& res)
+void _traverse(TreeNode *root, int& depth, int& res)
 {
     if (root == nullptr) {
         res = max(depth, res);
@@ -35,6 +94,6 @@ int main()
 {
     vector<int> case1 {3, 9, 20, -1, -1, 15, 7};
     TreeNode *tree1 = build_tree(case1);
-    cout << maxDepth(tree1) << endl;
+    cout << max_depth(tree1) << endl;
     return 0;
 }
