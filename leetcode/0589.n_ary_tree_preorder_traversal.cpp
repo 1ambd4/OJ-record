a02b42f927c080d81ec82676694d4a44b0883025
lttzz <luckdog.cdr@gmail.com>
2023-03-14 13:05:07 +0800

    leetcode: 0589 n-ary-tree-preorder-traversal

--- leetcode/0589.n_ary_tree_preorder_traversal.cpp
+++ leetcode/0589.n_ary_tree_preorder_traversal.cpp
@@ -5,26 +5,51 @@
 
 using namespace std;
 
-vector<int> res;
-
-void preorder_(Node* root)
+vector<int> preorder(Node *root)
 {
-    if (root == nullptr) return ;
+    vector<int> res;
+    if (root == nullptr) return res;
 
-    res.push_back(root->val);
-    for (const auto& child : root->children) {
-        preorder_(child);
-    }
-}
+    auto traverse = [&res](auto&& self, Node *root) -> void {
+        if (root == nullptr) return ;
 
-vector<int> preorder(Node* root)
-{
-    preorder_(root);
+        res.push_back(root->val);
+        for (const auto& child : root->children) {
+            self(self, child);
+        }
+    };
+
+    traverse(traverse, root);
+
     return res;
 }
 
+// 太不优雅啦
+// vector<int> res;
+//
+// void preorder_(Node* root)
+// {
+//     if (root == nullptr) return ;
+//
+//     res.push_back(root->val);
+//     for (const auto& child : root->children) {
+//         preorder_(child);
+//     }
+// }
+//
+// vector<int> preorder(Node* root)
+// {
+//     preorder_(root);
+//     return res;
+// }
+
 int main()
 {
+    vector<int> nums1 {1,-1,3,2,4,-1,5,6};
+    Node *tree1 = build_nbt(nums1);
+    vector<int> res1 = preorder(tree1);
+    cout << res1 << endl;
+
     return 0;
 }
 
