04b3b0f53079fb5bc5c88697c3f185d5bd6f69c5
lttzz <luckdog.cdr@gmail.com>
2023-03-15 10:07:30 +0800

    leetcode: 0530 minimum-absolute-difference-in-bst

--- leetcode/0530.minimum_absolute_difference_in_bst.cpp
+++ leetcode/0530.minimum_absolute_difference_in_bst.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <climits>
 
 #include "leetcode.h"
 
 using namespace std;
+
+int get_minimum_difference(TreeNode *root)
+{
+    int res = numeric_limits<int>::max(), pre = -1;
+
+    auto traverse = [&res, &pre](auto&& self, TreeNode *root) -> void {
+        if (root == nullptr) return ;
+
+        self(self, root->left);
+        if (pre != -1) res = min(res, root->val - pre);
+        pre = root->val;
+        self(self, root->right);
+    };
+
+    traverse(traverse, root);
+
+    return res;
+}
 
 void traverse(TreeNode *root, int& ans, int& pre)
 {
@@ -12,11 +31,11 @@ void traverse(TreeNode *root, int& ans, int& pre)
 
     traverse(root->left, ans, pre);
     if (pre != -1) ans = min(ans, root->val - pre);
-    pre = root->val; 
+    pre = root->val;
     traverse(root->right, ans, pre);
 }
 
-int get_minimum_difference(TreeNode *root)
+int _get_minimum_difference(TreeNode *root)
 {
     int ans = INT_MAX, pre = -1;
     traverse(root, ans, pre);
