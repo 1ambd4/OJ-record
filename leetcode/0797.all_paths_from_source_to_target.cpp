767b008b444c0b147d64c3c41678aa056034f613
lttzz <luckdog.cdr@gmail.com>
2023-03-15 11:18:55 +0800

    leetcode: 0797 all-paths-from-source-to-target

--- leetcode/0797.all_paths_from_source_to_target.cpp
+++ leetcode/0797.all_paths_from_source_to_target.cpp
@@ -3,6 +3,61 @@
 
 using namespace std;
 
+vector<vector<int>> all_paths_source_target(vector<vector<int>>& graph)
+{
+    vector<vector<int>> res;
+    vector<int> path;
+    int n = graph.size() - 1;
+
+    auto traverse = [&res, &path, &n](auto&& self, vector<vector<int>>& graph, int s) -> void {
+        if (s == n) {                       // 保存结果
+            res.push_back(path);
+            return ;
+        }
+
+        for (const auto& v : graph[s]) {    // 遍历所有的选择
+            path.push_back(v);              // 选择
+            self(self, graph, v);           // 递归
+            path.pop_back();                // 回溯，撤销选择
+        }
+    };
+
+    path.push_back(0);                      // 必然是从节点0开始的
+
+    traverse(traverse, graph, 0);
+
+    return res;
+}
+
+vector<vector<int>> _all_paths_source_target(vector<vector<int>>& graph)
+{
+    vector<vector<int>> res;
+    vector<int> path;
+    int n = graph.size();
+
+    auto traverse = [&res, &path, &n](auto&& self, vector<vector<int>>& graph, int s) -> void {
+        path.push_back(s);
+
+        if (s == n - 1) {
+            res.push_back(path);
+            path.pop_back();
+            return ;
+        }
+
+        for (const auto& v : graph[s]) {
+            self(self, graph, v);
+        }
+
+        path.pop_back();
+
+
+    };
+
+    traverse(traverse, graph, 0);
+
+    return res;
+}
+
 void traverse(vector<vector<int>>& graph, vector<vector<int>>& res,
         vector<int>& path, int s)
 {
@@ -21,7 +76,7 @@ void traverse(vector<vector<int>>& graph, vector<vector<int>>& res,
     path.pop_back();
 }
 
-vector<vector<int>> all_paths_source_target(vector<vector<int>>& graph)
+vector<vector<int>> __all_paths_source_target(vector<vector<int>>& graph)
 {
     vector<vector<int>> res;
     vector<int> path;
