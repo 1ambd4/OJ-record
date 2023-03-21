a4ee8f3fb25dca4b510443f18773b4af13b75705
lttzz <luckdog.cdr@gmail.com>
2023-03-21 20:37:04 +0800

    leetcode: 0053 maximum-subarray

--- leetcode/0053.maximum_subarray.cpp
+++ leetcode/0053.maximum_subarray.cpp
@@ -3,7 +3,21 @@
 #include <cassert>
 using namespace std;
 
-int max_subarray(vector<int>& nums) {
+int max_subarray(vector<int>& nums)
+{
+    int n = nums.size(), res = nums[0];
+    vector<int> dp(n, 0);
+
+    dp[0] = nums[0];
+    for (int i = 1; i < n; ++i) {
+        dp[i] = max(dp[i-1] + nums[i], nums[i]);
+        res = max(res, dp[i]);
+    }
+
+    return res;
+}
+
+int _max_subarray(vector<int>& nums) {
     int n = nums.size(), res = nums[0];
 
     vector<int> f(n, 0);
@@ -19,15 +33,13 @@ int max_subarray(vector<int>& nums) {
 
 int main(void) {
     vector<int> nums1 { -2, 1, -3, 4, -1, 2, 1, -5 };
-    assert(max_subarray(nums1) == 6);
-    
-    vector<int> nums2 { 5, 4 -1, 7, 8 };
-    assert(max_subarray(nums2) == 23);
-    
+    cout << max_subarray(nums1) << endl;
+
     vector<int> nums3 { 1, -1, -2 };
-    assert(max_subarray(nums3) == 1);
-    
-    cout << "Accepted by the test example!" << endl;
+    cout << max_subarray(nums3) << endl;
+
+    vector<int> nums2 { 5, 4 -1, 7, 8 };
+    cout << max_subarray(nums2) << endl;
 
     return 0;
 }
