9fb83b0aae4f6c49d7788968e6239cd25ba7728a
lttzz <luckdog.cdr@gmail.com>
2023-03-22 15:53:44 +0800

    leetcode: 0322 coin-change

--- leetcode/0322.coin_change.cpp
+++ leetcode/0322.coin_change.cpp
@@ -6,6 +6,21 @@ using namespace std;
 int coin_change(vector<int>& coins, int amount)
 {
     vector<int> dp(amount+1, amount+1);
+
+    dp[0] = 0;
+    for (int i = 1; i <= amount; ++i) {
+        for (const auto& coin : coins) {
+            if (i - coin < 0) continue;
+            dp[i] = min(dp[i], dp[i-coin]+1);
+        }
+    }
+
+    return dp[amount] == amount + 1 ? -1 : dp[amount];
+}
+
+int _coin_change(vector<int>& coins, int amount)
+{
+    vector<int> dp(amount+1, amount+1);
     dp[0] = 0;
 
     for (int i = 1; i <= amount; ++i) {
