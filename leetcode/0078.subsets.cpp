12e1e7f46418dd7f936dbe99c2e13f1fe832cad7
lttzz <luckdog.cdr@gmail.com>
2023-03-24 18:43:15 +0800

    leetcode: 0078 subsets

--- leetcode/0078.subsets.cpp
+++ leetcode/0078.subsets.cpp
@@ -1,24 +1,47 @@
 #include <iostream>
 #include <vector>
+#include "leetcode.h"
 
 using namespace std;
 
-void backtrace(vector<int>& nums, int s, vector<vector<int>>& res, vector<int>& track)
+vector<vector<int>> subset(vector<int>& nums)
+{
+    int n = nums.size();
+    vector<vector<int>> res;
+    vector<int> track;
+
+    auto backtrack = [&res, &n](auto&& self, vector<int>& nums, vector<int>& track, int id) -> void {
+        res.push_back(track);
+
+        for (int i = id; i < n; ++i) {
+            track.push_back(nums[i]);
+            self(self, nums, track, i + 1);
+            track.pop_back();
+        }
+    };
+
+    backtrack(backtrack, nums, track, 0);
+
+    return res;
+}
+
+
+void _backtrace(vector<int>& nums, int s, vector<vector<int>>& res, vector<int>& track)
 {
     res.push_back(vector<int>(track));
 
     for (int i = s; i < nums.size(); ++i) {
         track.push_back(nums[i]);
-        backtrace(nums, i+1, res, track);
+        _backtrace(nums, i+1, res, track);
         track.pop_back();
     }
 }
 
-vector<vector<int>> subset(vector<int>& nums)
+vector<vector<int>> _subset(vector<int>& nums)
 {
     vector<vector<int>> res;
     vector<int> track;
-    backtrace(nums, 0, res, track);
+    _backtrace(nums, 0, res, track);
     return res;
 }
 
@@ -26,12 +49,6 @@ int main()
 {
     vector<int> nums1{1,2,3};
     vector<vector<int>> res1 = subset(nums1);
-    for (const auto& es : res1) {
-        cout << '[';
-        for (const auto& e : es) {
-            cout << e << ',';
-        }
-        cout << ']' << endl;
-    }
+    cout << res1 << endl;
     return 0;
 }
