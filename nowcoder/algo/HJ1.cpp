d0f30513e37099b3bd26c26385586191dc48c7f9
lttzz <luckdog.cdr@gmail.com>
2023-04-06 22:45:56 +0800

    nowcoder: HJ1

---
+++ nowcoder/algo/HJ1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    // string s, t;
+    // getline(cin, s);
+    // stringstream ss(s);
+    // while (ss >> t) ;
+    // cout << t.size() << endl;
+
+    string s;
+    while (cin >> s);
+    cout << s.size() << endl;
+    return 0;
+}
