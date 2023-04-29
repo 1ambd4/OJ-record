60310a326ae50b8f42b655d553e4d445bf72ef76
lttzz <luckdog.cdr@gmail.com>
2023-04-29 12:33:15 +0800

    leetcode: 0169 majority-element

--- leetcode/0169.majority_element.cpp
+++ leetcode/0169.majority_element.cpp
@@ -4,7 +4,25 @@
 
 using namespace std;
 
-int majority_element(vector<int>& nums)
+int majority_element(std::vector<int>& nums)
+{
+    int n = nums.size();
+    if (n == 1) return nums[0];
+
+    int num = nums[0], cnt = 1;
+    for (int i {1}; i < n; ++i) {
+        if (nums[i] != num) {
+            if (cnt > 0) --cnt;
+            else num = nums[i], cnt = 1;
+        } else {
+            ++cnt;
+        }
+    }
+
+    return num;
+}
+
+int _majority_element(vector<int>& nums)
 {
     stack<int> s;
     for (const auto& num : nums) {
